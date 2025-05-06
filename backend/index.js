83d02f1ab2159b1b9636aa24b51cf7b6d8397d60
Yatharth <97800277+yatharth-2906@users.noreply.github.com>
2025-05-06 16:31:46 +0530

    updated cors

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,15 @@ const PORT = parseInt(process.env.PORT);
 
 connectToMongoDB();
 
-app.use(cors());
+const corsOptions = {
+  origin: function (origin, callback) {
+    callback(null, true);
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended:false}));
