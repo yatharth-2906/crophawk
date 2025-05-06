8d029cc9c5fbb42b09808a4db3d26f4e9d777e43
Yatharth <97800277+yatharth-2906@users.noreply.github.com>
2025-05-06 17:31:23 +0530

    Optimized API's

--- backend/index.js
+++ backend/index.js
@@ -34,11 +34,6 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-});
-
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended:false}));
